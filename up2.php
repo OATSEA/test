c3c6173943bdb30ef6aa9ad9c1a51024d7c170fa
OATSEA <harry@oatsea.org>
2015-04-01 18:46:42 +1100

    Create up2.php

---
+++ up2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Clone Git Repo</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+</head>
+<body class="main">
+<p>Update:</p>
+<?php
+  $shellcmd="git pull";
+  echo "<p>Command: ".$shellcmd."</p>";
+  
+  // echo exec('.$shellcmd.');
+  //passthru($shellcmd);
+  exec('git pull',$output, $return_val);
+  // print_r( $output );
+  var_dump($output);
+  echo "\n";
+  echo 'Error: '. $return_val ."\n";
+  echo "<p>Did it pull?</p><p> - Error 0 means all okay, but Error 1 means probably a rights issue!</p>";
+  require "testchange.php";
+  echo "<p>Testdata: ".$testdata."</p>";
+?>  
+
+</body>
+</html>
